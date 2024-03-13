9e7e295d258084158a2662252db56d8ead436f7a
Kieran Mellor <kieran.mellor@outlook.com>
2024-03-13 22:24:09 +0000

    feat: Expo styling fixes

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,10 +1,14 @@
 import HorizontalDatepicker from "@awrminkhodaei/react-native-horizontal-datepicker";
 import { LinearGradient } from "expo-linear-gradient";
 import {
+	ArrowLeftRight,
 	CheckIcon,
 	HandHeart,
 	HandHeartIcon,
+	LogInIcon,
+	LogOutIcon,
 	LucideIcon,
+	PlaneIcon,
 	User,
 } from "lucide-react-native";
 import React, { useEffect, useState } from "react";
@@ -58,53 +62,56 @@ export default function TabOneScreen() {
 	const timeline = [
 		{
 			id: 1,
-			content: "Applied to",
-			target: "Front End Developer",
-			href: "#",
-			date: "Sep 20",
-			datetime: "2020-09-20",
-			icon: User,
-			iconBackground: "bg-gray-400",
+			type: "main",
+			icon: LogInIcon,
+			iconBackground: "bg-sky-400",
+			aboveTitle: "09:00",
+			content: "Report",
+			belowTitle: "",
 		},
 		{
 			id: 2,
-			content: "Advanced to phone screening by",
-			target: "Bethany Blake",
-			href: "#",
-			date: "Sep 22",
-			datetime: "2020-09-22",
-			icon: HandHeart,
-			iconBackground: "bg-blue-500",
+			type: "main",
+			content: "London Gatwick (LGW) to Faro (FAO)",
+			aboveTitle: "10:00 - 12:05",
+			belowTitle: "U2 1234 | 2 hrs 5 mins | G-EZTL (A320)",
+			icon: PlaneIcon,
+			iconBackground: "bg-green-500",
+		},
+		{
+			id: 2.1,
+			type: "between",
+			content: "Turnaround - 35 mins",
+			aboveTitle: "",
+			belowTitle: "",
+			icon: ArrowLeftRight,
+			iconBackground: "bg-white",
 		},
 		{
 			id: 3,
-			content: "Completed phone screening with",
-			target: "Martha Gardner",
-			href: "#",
-			date: "Sep 28",
-			datetime: "2020-09-28",
-			icon: CheckIcon,
+			type: "main",
+			content: "Faro (FAO) to London Gatwick (LGW)",
+			aboveTitle: "12:35 - 14:00",
+			belowTitle: "U2 1235 | 2 hrs 25 mins | G-EZTL (A320)",
+			icon: PlaneIcon,
 			iconBackground: "bg-green-500",
 		},
-		{
-			id: 4,
-			content: "Advanced to interview by",
-			target: "Bethany Blake",
-			href: "#",
-			date: "Sep 30",
-			datetime: "2020-09-30",
-			icon: HandHeartIcon,
-			iconBackground: "bg-blue-500",
-		},
+		// {
+		// 	id: 4,
+		// 	content: "Advanced to interview by",
+		// 	aboveTitle: "Bethany Blake",
+		// 	belowTitle: "Below title",
+		// 	icon: HandHeartIcon,
+		// 	iconBackground: "bg-blue-500",
+		// },
 		{
 			id: 5,
-			content: "Completed interview with",
-			target: "Katherine Snyder",
-			href: "#",
-			date: "Oct 4",
-			datetime: "2020-10-04",
-			icon: CheckIcon,
-			iconBackground: "bg-green-500",
+			type: "main",
+			content: "Off Duty",
+			aboveTitle: "14:30",
+			belowTitle: "",
+			icon: LogOutIcon,
+			iconBackground: "bg-sky-500",
 		},
 	];
 
@@ -206,7 +213,7 @@ export default function TabOneScreen() {
 
 	return (
 		<View className="flex-1 bg-background">
-			<ScrollView className="gap-y-4">
+			<ScrollView className="gap-y-4" alwaysBounceVertical={false}>
 				{/* <View
 					style={{ width: "100%", height: 350, backgroundColor: "#0EA5E9" }}
 				> */}
@@ -217,7 +224,7 @@ export default function TabOneScreen() {
 					style={{ width: "100%", height: 370 }}
 					// style={styles.background}
 				>
-					<View className="mt-[126px]" style={{ flexDirection: "row" }}>
+					<View style={{ flexDirection: "row", marginTop: 126 }}>
 						<View style={{ flex: 1 }}>
 							<Text
 								style={{ fontFamily: "PlayfairDisplay_800ExtraBold" }}
@@ -231,12 +238,12 @@ export default function TabOneScreen() {
 								Take a look at your roster below.
 							</Text>
 						</View>
-						<View className="mr-5 -mb-[40px]" style={{ flex: 1 }}>
+						<View className="mr-5" style={{ flex: 1, marginBottom: -40 }}>
 							{/* <Plane /> */}
 							<EZYA320 />
 						</View>
 					</View>
-					<View className="-mb-[54px] -mt-[70px]">
+					<View style={{ marginTop: -70, marginBottom: -54 }}>
 						<CloudsImage />
 					</View>
 				</LinearGradient>
@@ -274,27 +281,48 @@ export default function TabOneScreen() {
 							<View key={event.id}>
 								<View className="relative pb-8">
 									{eventIdx !== timeline.length - 1 ? (
-										<View className="absolute left-[17px] top-8 -ml-px h-full w-1 bg-gray-200" />
+										<View
+											style={{ left: 17 }}
+											className="absolute top-8 -ml-px h-full w-1 bg-gray-200"
+										/>
 									) : null}
 									<View className="relative flex space-x-3">
 										<View>
 											<View
 												className={cn(
 													event.iconBackground,
-													"h-[36px] w-[36px] rounded-full flex items-center justify-center ring-8 ring-white",
+													"h-12 w-12 rounded-full flex items-center justify-center ring-8 ring-white",
 												)}
 											>
-												<event.icon size={24} color="white" />
+												<event.icon
+													size={event.type === "main" ? 24 : 20}
+													color={event.type === "main" ? "white" : "#6b7280"}
+												/>
 											</View>
 										</View>
-										<View className="flex min-w-0 flex-1 justify-between space-x-4 pl-[50px] -mt-[30px]">
+										<View
+											style={{ paddingLeft: 50, marginTop: -38 }}
+											className="flex min-w-0 flex-1 justify-between space-x-4"
+										>
 											<View>
 												<Text className="text-lg text-gray-500">
-													{event.content}{" "}
-													<Text className="font-medium text-gray-900">
-														{event.target}
-													</Text>
+													{event.aboveTitle}
 												</Text>
+												<Text
+													style={{ marginTop: -6 }}
+													className={
+														event.type === "main"
+															? "text-lg font-medium text-gray-900"
+															: "text-lg font-medium text-gray-500"
+													}
+												>
+													{event.content}
+												</Text>
+												{event.belowTitle.length > 1 && (
+													<Text className="text-lg text-gray-600">
+														{event.belowTitle}
+													</Text>
+												)}
 											</View>
 											{/* <Text className="whitespace-nowrap text-right text-md text-gray-500">
 												{event.date}
